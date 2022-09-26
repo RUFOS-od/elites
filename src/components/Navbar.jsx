6c66f3ecf93d8fc372e440c0ffde49db23580e4d
Henri Baudouin  Komena <hkomena@gmail.com>
2022-09-26 06:55:09 +0000

    mise à jour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       <h1 className="w-full text-3xl font-bold text-[#9b189e]">Elite.</h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
+        <li className="p-4">HomeH</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
